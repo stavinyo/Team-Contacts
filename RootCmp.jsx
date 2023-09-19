4ed9602a58ec422063a3e457715ad35cf5b7d6c5
Tal Cohen <talco4088@gmail.com>
2023-09-19 19:40:30 +0300

    test2

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -19,8 +19,9 @@ export function App() {
                         <Route path="/" element={<Home />} />
                         <Route path="/about" element={<About />} />
                         <Route path="/contact" element={<ContactIndex />} />
+                        {/* <Route path="/contact/:contactId" element={<ContactDetails />} /> */}
                         <Route path="/contact/:contactId" element={<ContactDetails />} />
-                        
+
                     </Routes>
                 </section>
             </Router>
