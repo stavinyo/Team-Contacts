ebeb08e0153fec9ed702c1b5377d521833c66e4b
Tal Cohen <talco4088@gmail.com>
2023-09-19 21:32:13 +0300

    F/contact-details-cmp/add this cmp

--- cmps/ContactDetails.jsx
+++ cmps/ContactDetails.jsx
@@ -1,6 +1,10 @@
 
-export function ContactDetails() {
+export function ContactDetails({contact}) {
+
+
     return <section className="contact-details-container">
-        <h1>Welcome to Contact Details</h1>
+        <h1>{contact.firstName + ' ' + contact.lastName}</h1>
+        <p>{'email: ' + contact.email}</p>
+        <p>{'phone: ' + contact.phone}</p>
     </section>
 }
