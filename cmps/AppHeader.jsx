9bcef0e53b557e3ea05c9446187985fc03d0703f
Stav Cohen <stavco28@gmail.com>
2023-09-19 16:36:52 +0300

    styling nav bar

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -2,11 +2,11 @@ const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
     return (
-        <header className="app-header">
+        <header className="app-header-container">
             <Link to="/">
                 <h3>LOGO!</h3>
             </Link>
-            <nav>
+            <nav className="nav-link-container">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/about">About</NavLink>
                 <NavLink to="/contact">Todo</NavLink>
